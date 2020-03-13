0f7f17a21b93e490b53d7ec56c4a6ac2c55567f3
Cairo Vieira <cairovieira@MacBook-Pro-de-Cairo.local>
2020-03-13 20:52:16 -0300

    granger v1

---
+++ precache-manifest.82e747f9315cfb06c7437fc3b3035413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12090fe04e9d8d3d32af48142cc74cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4de72e51ef988c485ca1",
+    "url": "/static/css/main.9b193454.chunk.css"
+  },
+  {
+    "revision": "149143d9c1aec380ff8b",
+    "url": "/static/js/2.bbd2be6b.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/static/js/2.bbd2be6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de72e51ef988c485ca1",
+    "url": "/static/js/main.a856a35b.chunk.js"
+  },
+  {
+    "revision": "7f675cefc8a7efc90545",
+    "url": "/static/js/runtime-main.2c77c7c6.js"
+  }
+]);
