27d009a0da9502cad170293084576c3f295672f0
Cairo Vieira <cairovieira@MacBook-Pro-de-Cairo.local>
2020-03-14 15:00:25 -0300

    v1.0.1

---
+++ precache-manifest.36cfa7c80bc382f63e5e7e20a342a2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c560953bd7ca4631211054fecca78bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca65cf60086117238496",
+    "url": "/static/css/main.9b193454.chunk.css"
+  },
+  {
+    "revision": "0f0924c112642414e1fd",
+    "url": "/static/js/2.dcc6e2f2.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/static/js/2.dcc6e2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca65cf60086117238496",
+    "url": "/static/js/main.86ae4522.chunk.js"
+  },
+  {
+    "revision": "7f675cefc8a7efc90545",
+    "url": "/static/js/runtime-main.2c77c7c6.js"
+  }
+]);
