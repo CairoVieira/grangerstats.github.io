ec606eeed40c32ea017af62f9ed049bd8c6e9e6f
Cairo Vieira <cairovieira@MacBook-Pro-de-Cairo.local>
2020-03-29 18:58:59 -0300

    v1.0.4

---
+++ precache-manifest.2118488ef1729bde0bd7aae3f3da996d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76aaf969ed092d298e81d0925e97807f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bfe0a7066851e68a30e",
+    "url": "/static/css/main.9c3c0301.chunk.css"
+  },
+  {
+    "revision": "e6ebcfe0716f1ea72257",
+    "url": "/static/js/2.7171e3c9.chunk.js"
+  },
+  {
+    "revision": "291235bb35e20bb45bc460d590a63bd8",
+    "url": "/static/js/2.7171e3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bfe0a7066851e68a30e",
+    "url": "/static/js/main.a3ed73c6.chunk.js"
+  },
+  {
+    "revision": "7f675cefc8a7efc90545",
+    "url": "/static/js/runtime-main.2c77c7c6.js"
+  },
+  {
+    "revision": "d2bc4d609356ba996819f0fdeafaaab6",
+    "url": "/static/media/CR.d2bc4d60.png"
+  },
+  {
+    "revision": "4970eb2d0a5d254b3ab4234077ae90ae",
+    "url": "/static/media/ED.4970eb2d.png"
+  },
+  {
+    "revision": "7fa59ad67f4450faefb7ad2443848116",
+    "url": "/static/media/Prob.7fa59ad6.png"
+  },
+  {
+    "revision": "16829979b7bad7283552d205df4b0dc0",
+    "url": "/static/media/data.16829979.svg"
+  },
+  {
+    "revision": "93a0ae7367b68517173806a02e3b0460",
+    "url": "/static/media/main.93a0ae73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.660863b277a35f1185693507e7c4e2f2.js"
+  "/precache-manifest.2118488ef1729bde0bd7aae3f3da996d.js"
 );
 
 self.addEventListener('message', (event) => {
